69feb418012a84317be5cabfc8c6ecee2f7d5314
wg18618262234 <wg18618262234@outlook.com>
2018-11-21 15:56:47 +0800

    新增日志模块

--- common/logger.py
+++ common/logger.py
@@ -1,6 +1,27 @@
 import logging
 
-if __name__ == '__main__':
-    logger = logging.getLogger('mulogger')
-    logger.setLevel(logging.DEBUG)
 
+def logger(level, msg):
+    LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+    DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
+
+    logging.basicConfig(level=logging.DEBUG, filename='../logs/my.log', format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+    if 'debug' in level:
+        logging.debug(msg)
+    elif 'info' in level:
+        logging.info(msg)
+    elif 'warning' in level:
+        logging.warning(msg)
+    elif 'error' in level:
+        logging.error(msg)
+    elif 'critical' in level:
+        logging.critical(msg)
+    else:
+        logging.debug('error type of log !')
+        return False
+
+
+
+if __name__ == '__main__':
+    logger('debug','test')
